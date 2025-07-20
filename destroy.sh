0f89cc20cb42bbc3fddb82fd9b9d17dc14f4191e
Leo <liusongdu@hotmail.com>
2025-07-20 14:59:32 +0800

    Fix InvokeInlineAgent API call throttling

--- destroy.sh
+++ destroy.sh
@@ -1,4 +1,4 @@
 source .venv/bin/activate
-cdk destroy --all --require-approval never
-rm -rf .venv
+cdk destroy --all --require-approval never --profile leo@539247484506
+#rm -rf .venv
 rm -rf cdk.out
